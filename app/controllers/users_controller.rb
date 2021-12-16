78312bf68682d06ec5e7bdacd28432c5a7985311
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-16 11:12:55 +0900

    ユーザー情報を他ユーザーが編集できないように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :login_required, only: %i[new create]
 
   # GET /users or /users.json
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.new
+    @user.user_name = current_user.user_name
+    @user.email = current_user.email
   end
 
   # POST /users or /users.json
@@ -32,23 +33,29 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if @user.id == current_user.id
+      respond_to do |format|
+        @user.update_column(:email, params[:user][:email].downcase)
+        if @user.update(update_params)
+          
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
+    if @user.id == current_user.id
+      @user.destroy
+      respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
+      end
     end
   end
 
@@ -64,6 +71,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(
       :user_name,
       :email,
+      :password,
+      :password_confirmation,
+      :my_image
+    )
+  end
+
+  def update_params
+    params.require(:user).permit(
+      :user_name,
       :password,
       :password_confirmation,
       :my_image
