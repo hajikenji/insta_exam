acb329c5c7cbbfb348711ade116a71ac65d11ae1
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-16 15:40:12 +0900

    マイグレーションファイル調整。エラーを吐く過去マイグレ削除

--- db/migrate/20211214155752_rename_picture_commnet.rb
+++
@@ -1,5 +0,0 @@
-class RenamePictureCommnet < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :pictures, :commnet, :comment
-  end
-end
--- db/migrate/20211215024248_add_column.rb
+++
@@ -1,5 +0,0 @@
-class AddColumn < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :my_image, :text
-  end
-end
